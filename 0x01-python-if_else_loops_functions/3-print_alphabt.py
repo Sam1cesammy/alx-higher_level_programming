130a4703645832c5c53a014e25fbbe8eb778a579
Sam1cesammy <samuel.asegame@gmail.com>
2023-11-29 14:59:38 -0800

    my alx task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
-        continue
-    print(chr(i).format(), end="")
+for x in range(97, 123):
+    if x != 101 and x != 113:
+        print("{:c}".format(x), end="")
 
