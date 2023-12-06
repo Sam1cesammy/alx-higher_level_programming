1f0dda57a317a45583904c8a8c0c292bcb534844
Sam1cesammy <samuel.asegame@gmail.com>
2023-12-05 20:56:54 -0800

    my alx task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+    if idx >= 0 and idx < len(my_list):
+        return (my_list.pop(idx))
+    else:
+        return (None)
