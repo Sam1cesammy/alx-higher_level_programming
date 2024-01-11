c21842850422a00643806ee3a6576e384853dd8e
Sam1cesammy <samuel.asegame@gmail.com>
2024-01-10 20:04:00 -0800

    my alx task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,9 @@
 
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
+
     Float arguments are typecasted to ints before addition is performed.
+
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
@@ -13,3 +15,7 @@ def add_integer(a, b=98):
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
